7ced838af156a4a8ab3d0de276d900e2ab69b97b
KANISHKAJOSHI41 <joshikanishka24605@gmail.com>
2024-07-30 12:39:45 +0530

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,19 @@
+// class  welcome {
+//   public static void main(String[] args)
+//    {
+//        System.out.println ("hello java");
+//   }
+    
+// }
+
+class Conversion{
+    public static void main(String[] args) {
+        float b;
+        char i= 'a';
+
+        System.out.println("\nchar to float" );
+        b = (float) i ;
+        System.out.println("i and b " +i+" and "+b);
+
+    }
+}
